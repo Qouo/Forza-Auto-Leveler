71f0fd5f31bc2a920c6f4dab0def895edbb41d9a
Will Soteros <42285360+Qouo@users.noreply.github.com>
2023-12-11 23:06:48 -0500

    Update forza SUCKS.py
    made the program actually work LOL

--- forza SUCKS.py
+++ forza SUCKS.py
@@ -1,54 +1,61 @@
-# TODO -- add image of what the car level 50 should look like for the program to stop.
-
 import time
 import pyautogui
 import logging
-import cv2
-import numpy as np
-from io import BytesIO
-from PIL import Image
+import threading
+import tkinter as tk
+from tkinter import font
 
 # Configure logging
 logging.basicConfig(filename='key_press.log', level=logging.INFO, format='%(asctime)s - %(message)s')
 
+# Global variable to control the loop
+running = False
+
 def press_key():
-    # Simulate pressing the "D" key
-    pyautogui.press('d')
+    while running:
+        # Simulate pressing the "D" key
+        pyautogui.press('d')
 
-    # Log the key press
-    logging.info('"D" key pressed at %s', time.strftime('%Y-%m-%d %H:%M:%S'))
+        # Log the key press
+        logging.info('"D" key pressed at %s', time.strftime('%Y-%m-%d %H:%M:%S'))
 
-def check_game_level():
-    # Capture the screen
-    screenshot = pyautogui.screenshot()
-
-    # Load the template image from the bundled data
-    template_data = open("level_50_template.png", "rb").read()
-    template_image = Image.open(BytesIO(template_data))
-
-    # Convert the template image to a NumPy array
-    template = np.array(template_image)
-
-    # Convert the screenshot to a NumPy array
-    screenshot_np = np.array(screenshot)
-
-    # Convert the screenshot to grayscale
-    screenshot_gray = cv2.cvtColor(screenshot_np, cv2.COLOR_BGR2GRAY)
-
-    # Perform template matching
-    result = cv2.matchTemplate(screenshot_gray, template, cv2.TM_CCOEFF_NORMED)
-
-    # Set a threshold for matching
-    threshold = 0.8
-    loc = np.where(result >= threshold)
-
-    # Check if any matches are found
-    return len(loc[0]) > 0
-
-if __name__ == "__main__":
-    # Run the script while the game level is not 50
-    while not check_game_level():
-        press_key()
+        # Sleep for 5 minutes
         time.sleep(300)  # 300 seconds = 5 minutes
 
-    print("Game reached level 50. Stopping the script.")
+def start():
+    global running
+    if not running:
+        running = True
+        # Log that the program has started
+        logging.info('Program started at %s', time.strftime('%Y-%m-%d %H:%M:%S'))
+        # Start a new thread for the key press function
+        threading.Thread(target=press_key).start()
+
+def stop():
+    global running
+    running = False
+    # Log that the program has stopped
+    logging.info('Program stopped at %s', time.strftime('%Y-%m-%d %H:%M:%S'))
+
+if __name__ == "__main__":
+    # Create the main window
+    root = tk.Tk()
+    root.title("Forza Sucks")
+
+    # Comic Sans font
+    comic_sans_font = font.Font(family="Comic Sans MS", size=12)
+
+    # Program name label
+    program_label = tk.Label(root, text="Forza FUCKING sucks", font=comic_sans_font)
+    program_label.pack(pady=10)
+
+    # Start button
+    start_button = tk.Button(root, text="Start", command=start)
+    start_button.pack(pady=10)
+
+    # Stop button
+    stop_button = tk.Button(root, text="Stop", command=stop)
+    stop_button.pack(pady=10)
+
+    # Run the main loop
+    root.mainloop()
